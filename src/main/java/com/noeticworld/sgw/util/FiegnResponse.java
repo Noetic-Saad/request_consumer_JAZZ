086370f4d85d702e958d82cf856679c5b3651e9e
Furqan Ghauri <furqan@noeticworld.com>
2019-12-30 15:02:25 +0500

    changes before unsub

---
+++ src/main/java/com/noeticworld/sgw/util/FiegnResponse.java
@@ -0,0 +1,32 @@
+package com.noeticworld.sgw.util;
+
+public class FiegnResponse {
+
+    private int code;
+    private String msg;
+    private String correlationId;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getCorrelationId() {
+        return correlationId;
+    }
+
+    public void setCorrelationId(String correlationId) {
+        this.correlationId = correlationId;
+    }
+}
