480614c707013d1bf8e96655da1909c42b5255ea
Rizwan <rizwan@noeticworld.com>
2020-03-16 17:24:17 +0500

    sdfsdfsd

--- src/main/java/com/noeticworld/sgw/requestConsumer/repository/VendorRequestRepository.java
+++ src/main/java/com/noeticworld/sgw/requestConsumer/repository/VendorRequestRepository.java
@@ -2,7 +2,9 @@ package com.noeticworld.sgw.requestConsumer.repository;
 
 import com.noeticworld.sgw.requestConsumer.entities.VendorRequestsStateEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VendorRequestRepository extends JpaRepository<VendorRequestsStateEntity, Long> {
 
     VendorRequestsStateEntity findByCorrelationid(String correlationId);
