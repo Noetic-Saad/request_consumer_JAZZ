b4a84a8a41911ef44caf6081503b676a5c82d7ff
noetic-habib <habib@noeticworld.com>
2020-11-06 14:10:49 +0500

    sdfsd

--- src/main/java/com/noeticworld/sgw/requestConsumer/service/externalEvents/SubscriptionEventHandler.java
+++ src/main/java/com/noeticworld/sgw/requestConsumer/service/externalEvents/SubscriptionEventHandler.java
@@ -149,7 +149,8 @@ public class SubscriptionEventHandler implements RequestEventHandler {
         }
         usersStatusEntity.setAttempts(1);
         usersStatusEntity.setUserId(_user.getId());
-        usersStatusEntity.setFreeTrialExpiry(Timestamp.valueOf(LocalDate.now().plusDays(2).atTime(23, 59)));
+      //  usersStatusEntity.setFreeTrialExpiry(Timestamp.valueOf(LocalDate.now().plusDays(2).atTime(23, 59)));
+        usersStatusEntity.setFreeTrialExpiry(Timestamp.from(Instant.now()));
         userStatusRepository.save(usersStatusEntity);
         updateUserStatus(_user, usersStatusEntity.getId(),requestProperties.getVendorPlanId());
        // userStatusRepository.flush();
