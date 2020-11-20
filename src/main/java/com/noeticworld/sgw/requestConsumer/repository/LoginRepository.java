386414ed6e68452d7643a23d7a53c1ecc129dc38
noetic-habib <habib@noeticworld.com>
2020-11-20 10:00:15 +0500

    @transactional Annotation added to update records

--- src/main/java/com/noeticworld/sgw/requestConsumer/repository/LoginRepository.java
+++ src/main/java/com/noeticworld/sgw/requestConsumer/repository/LoginRepository.java
@@ -7,9 +7,11 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
 public interface LoginRepository extends JpaRepository<LoginEntity,Integer> {
-
+    @Transactional
     @Modifying
     @Query(value = "update login set code = '0' where id = (select id from login where msisdn=:msisdn order by id desc limit 1)",nativeQuery = true)
     void updateLoginTable(@Param("msisdn") long msisdn);
