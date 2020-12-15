fe10b9cfc044da050bd7f156be533bf245b448c9
noetic-habib <habib@noeticworld.com>
2020-12-15 16:55:02 +0500

    test

--- src/main/java/com/noeticworld/sgw/requestConsumer/repository/UserStatusRepository.java
+++ src/main/java/com/noeticworld/sgw/requestConsumer/repository/UserStatusRepository.java
@@ -17,9 +17,9 @@ interface UserStatusRepository extends JpaRepository<UsersStatusEntity,Integer> 
     UsersStatusEntity findTopById(long id);
     @Query(value = "SELECT * FROM public.users_status WHERE user_id=:id and free_trial>:fromDate",nativeQuery = true)
     UsersStatusEntity returnUserExpiredOrnOt(@Param("id") Long id, @Param("fromDate") Timestamp fromDate);
-    @Query(value = "SELECT * from public.user_status where expiryDate<:cdate" ,nativeQuery = true)
+    @Query(value = "SELECT * from public.users_status where expiryDate<:cdate" ,nativeQuery = true)
     UsersStatusEntity GetFreeTrial(@Param("cdate") Timestamp cdate);
-    @Query(value = "SELECT status_id from public.user_status where id:id order by id desc" ,nativeQuery = true)
+    @Query(value = "SELECT status_id from public.users_status where id:id order by id desc" ,nativeQuery = true)
     int UnsubStatus(@Param("id") long id);
 
 }
